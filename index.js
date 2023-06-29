77c4694fdb5ec55b3f40a705c9851a4f966fb86b
Vytautas Gumbakis <vgumbakis@gmail.com>
2023-06-29 21:15:34 +0300

    initial index file and structure

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const ticketRouter = require('./routes/answer');
+const user = require('./routes/user');
+
+const app = express();
+
+require('dotenv').config();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(ticketRouter);
+app.use(user);
+
+mongoose
+  .connect(process.env.MONGO_CONNECT)
+  .then(() => {
+    console.log('CONNECTED');
+  })
+  .catch((err) => {
+    console.log('err', err);
+  });
+
+app.listen(process.env.PORT, () => {
+  console.log('Stack overflow clone app is alive!!!!!');
+});
