d36627303c5b0dd287e4266f07a0236d1c301ddb
Vytautas Gumbakis <vgumbakis@gmail.com>
2023-07-08 14:15:07 +0300

    final

--- controllers/question.js
+++ controllers/question.js
@@ -1,5 +1,6 @@
 const QuestionModel = require('../models/question');
 const uniqid = require('uniqid');
+
 module.exports.GET_ALL_QUESTIONS_CONTROLLER = async (req, res) => {
   try {
     const questions = await QuestionModel.find().sort({ title: 1 });
@@ -27,6 +28,7 @@ module.exports.POST_QUESTION_CONTROLLER = async (req, res) => {
       id: uniqid(),
       title: req.body.title,
       content: req.body.content,
+      answers: [],
     });
 
     await question.save();
@@ -40,11 +42,9 @@ module.exports.DELETE_QUESTION_BY_ID_CONTROLLER = async (req, res) => {
   try {
     const questionId = req.params.id;
 
-    // Find the question by ID and delete it
     const result = await QuestionModel.deleteOne({ id: questionId });
 
     if (result.deletedCount === 0) {
-      // No question was deleted
       return res.status(404).json({ response: 'Question not found' });
     }
 
@@ -60,29 +60,19 @@ module.exports.POST_ANSWER_FOR_QUESTION_CONTROLLER = async (req, res) => {
     const questionId = req.params.id;
     const { content } = req.body;
 
-    // Find the question by ID
     const question = await QuestionModel.findOne({ id: questionId });
 
     if (!question) {
-      // Question not found
       return res.status(404).json({ response: 'Question not found' });
     }
 
-    // Initialize the answers array if it's undefined
-    if (!question.answers) {
-      question.answers = [];
-    }
-
-    // Create a new answer for the question
     const answer = {
       id: uniqid(),
       content,
     };
 
-    // Add the answer to the question's answers array
     question.answers.push(answer);
 
-    // Save the updated question
     await question.save();
 
     res.status(200).json({ response: 'Success, answer added to question' });
@@ -93,36 +83,29 @@ module.exports.POST_ANSWER_FOR_QUESTION_CONTROLLER = async (req, res) => {
 };
 
 module.exports.DELETE_ANSWER_BY_ID_CONTROLLER = async (req, res) => {
-  try {
-    const answerId = req.params.id;
-    const questionId = req.query.questionId;
+  const { id } = req.params;
 
-    // Find the question by ID
-    const question = await QuestionModel.findOne({ id: questionId });
+  try {
+    let question = await QuestionModel.findOne({ 'answers.id': id });
 
     if (!question) {
-      // Question not found
-      return res.status(404).json({ response: 'Question not found' });
+      return res.status(404).json({ message: 'Answer not found.' });
     }
 
-    // Find the index of the answer within the question's answers array
-    const answerIndex = question.answers.findIndex((answer) => answer.id === answerId);
+    const updatedAnswers = question.answers.filter((answer) => answer.id !== id);
 
-    if (answerIndex === -1) {
-      // Answer not found
-      return res.status(404).json({ response: 'Answer not found' });
+    if (updatedAnswers.length === question.answers.length) {
+      return res.status(404).json({ message: 'Answer not found.' });
     }
 
-    // Remove the answer from the question's answers array
-    question.answers.splice(answerIndex, 1);
+    question.answers = updatedAnswers;
 
-    // Save the updated question
-    await question.save();
+    question = await question.save();
 
-    res.status(200).json({ response: 'Success, answer deleted' });
-  } catch (err) {
-    console.log('err', err);
-    res.status(500).json({ response: 'Failure, answer was not deleted' });
+    return res.status(200).json({ message: 'Answer deleted successfully.' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal server error.' });
   }
 };
 
@@ -130,15 +113,12 @@ module.exports.GET_ALL_ANSWERS_FOR_QUESTION_BY_ID_CONTROLLER = async (req, res) 
   try {
     const questionId = req.params.id;
 
-    // Find the question by ID
     const question = await QuestionModel.findOne({ id: questionId });
 
     if (!question) {
-      // Question not found
       return res.status(404).json({ response: 'Question not found' });
     }
 
-    // Retrieve all answers for the question
     const answers = question.answers;
 
     res.status(200).json({ response: answers });
--- models/question.js
+++ models/question.js
@@ -4,6 +4,7 @@ const questionSchema = mongoose.Schema({
   id: { type: String, required: true, min: 3 },
   title: { type: String, required: true, min: 3 },
   content: { type: String, required: true, min: 8 },
+  answers: { type: Array, default: [] },
 });
 
 module.exports = mongoose.model('Question', questionSchema);
